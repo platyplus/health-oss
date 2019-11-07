492e52aa862dd0ba186df2632b2b46d7b81ea32b
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-11-07 13:37:47 +0100

    structure

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  title: 'Health Open-Source Sofware',
+  title: 'Health Open-Source Software',
   description:
-    'Open-source software for health interventions in low and middle-income countries'
+    'Open-source software for health interventions in low and middle-income countries',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Introduction', link: '/introduction' }
+    ]
+  }
 }
